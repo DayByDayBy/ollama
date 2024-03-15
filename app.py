65b282d7fa311db2919abadddb407690a55792cc
Diomedes Git <diomedesgit@gmail.com>
2024-03-15 01:30:20 +0000

    message testing

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 import ollama
+import numpy as np
+import streamlit as st
 
 
-response = ollama.chat(model='gemma', messages=[
+# with st.chat_message("assistant"):
+#     st.write("Hello human")
+#     st.bar_chart(np.random.randn(30, 3))
+
+
+response1 = ollama.chat(model='gemma', messages=[
   {
     'role': 'user',
     'content': 'if we do, what shall we want?',
   },
 ])
-print(response['message']['content'])
+
+response2 = ollama.chat(model='llama2', messages=[
+  {
+    'role': 'user',
+    'content': 'if we do, what shall we want?',
+  },
+])
+# print(response1['message']['content'])
+# print(response2['message']['content'])
+
+
+
+with st.chat_message("user"):
+    st.write({response1['message']['content']})
+    
+with st.chat_message("admin"):
+    st.write({response2['message']['content']})
