03d7f4f80aa4936020e3fb57a9c80ecafb3450c6
Diomedes Git <diomedesgit@gmail.com>
2024-03-15 19:28:12 +0000

    nlu test

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 import ollama
+import nlu
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
 
-
-# with st.chat_message("assistant"):
-#     st.write("Hello human")
-#     st.bar_chart(np.random.randn(30, 3))
-
-
-response1 = ollama.chat(model='gemma', messages=[
-  {
-    'role': 'user',
-    'content': 'if we do, what shall we want?',
-  },
-])
-
-response2 = ollama.chat(model='llama2', messages=[
-  {
-    'role': 'user',
-    'content': 'if we do, what shall we want?',
-  },
-])
-# print(response1['message']['content'])
-# print(response2['message']['content'])
-
-
-
-with st.chat_message("user"):
-    st.write({response1['message']['content']})
     
-with st.chat_message("admin"):
-    st.write({response2['message']['content']})
+prompt  = 'if we do, what shall we want?'
+
+
+# # with st.chat_message("assistant"):
+# #     st.write("Hello human")
+# #     st.bar_chart(np.random.randn(30, 3))
+
+
+response1 = ollama.chat(
+    model='gemma', 
+    messages=[
+    {
+    'role': 'user',
+    'content': prompt,
+    },
+])
+
+response2 = ollama.chat(
+    model='llama2', 
+    messages=[
+    {
+    'role': 'user',
+    'content': prompt,
+    },
+])
+
+# with st.chat_message("user"):
+#     st.write(response1['message']['content'])
+    
+# with st.chat_message("admin"):
+#     st.write(response2['message']['content'])
+
+nlu.load('sentiment').predict('I love NLU! <3') 
+
+# col1, col2, col3 = st.columns([3, 5, 5])
+# with col2:
+#     st.write(response1['message']['content'])
+# with col3:
+#     st.write(response2['message']['content'])
+    
+    
