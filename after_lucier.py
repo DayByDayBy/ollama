c8d0fc5c4ea09b684cd6d78cf18db4f8ac6fc8c6
Diomedes Git <diomedesgit@gmail.com>
2024-03-17 23:52:26 +0000

    after lucier

--- after_lucier.py
+++ after_lucier.py
@@ -1,51 +1,42 @@
 import ollama
 import streamlit as st
 
-    
-prompt = '''as a prompt engineer, write an interesting prompt for an LLM. it should be framed as a request for an interesting prompt'''
+lucier_quote = '''"I am sitting in a room different 
+from the one you are in now. I am recording the sound 
+of my speaking voice and I am going to play it back 
+into the room again and again until the resonant 
+frequencies of the room reinforce themselves so that any 
+semblance of my speech, with perhaps the exception of rhythm, 
+is destroyed. What you will hear, then, are the natural resonant 
+frequencies of the room articulated by speech. I regard this activity 
+not so much as a demonstration of a physical fact, but more as 
+a way to smooth out any irregularities my speech might have."   -  Alvin Lucier'''
+
 epochs = 7
+initial_prompt = "as a prompt engineer, write an interesting prompt for an LLM. it should be framed as a request for an interesting prompt"
+responses = []
 
-def asker(initial_prompt, epochs):
-    prompt = initial_prompt
-    
-    for _, in range(epochs): 
-        response = ollama.generate(prompt)
+
+def asker(starter, epochs):
+    for _ in range(epochs): 
+        response = ollama.generate(
+            model = "llama2",
+            prompt = starter
+            )  
         st.write(f"response {_ + 1}: {response}")
-        prompt = response
-        
-col1, col2, col3 = st.columns([3, 5, 5])
-with col2:
-    st.write(response['message']['content'])
-    
+        responses.append(response)
+        prompt = response['message']
 
+def main():
+    st.title("after alvin lucier \n\n")
+    st.write(lucier_quote)
+    st.write("the starter-prompt: ", initial_prompt + "\n")
+    if st.button('roll the reel'):
+        asker(initial_prompt, epochs)
+        st.header("responses: ")
+        for idx, response in enumerate(responses):
+            st.write(f"response number {idx +1}: {response}")
+            
+if __name__ == "__main__":
+    main()
 
-
-# response1 = ollama.chat(
-#     model='gemma', 
-#     messages=[
-#     {
-#     'role': 'user',
-#     'content': prompt2,
-#     },
-# ])
-
-# response2 = ollama.chat(
-#     model='llama2', 
-#     messages=[
-#     {
-#     'role': 'user',
-#     'content': prompt2,
-#     },
-# ])
-
-
-
-
-
-
-
-# with st.chat_message("user"):
-#     st.write(response1['message']['content'])
-    
-# with st.chat_message("admin"):
-#     st.write(response2['message']['content'])
