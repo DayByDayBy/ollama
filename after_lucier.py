3d0e6876c193ce5d791d703da6c6ac6af227a74a
Diomedes Git <diomedesgit@gmail.com>
2024-06-18 22:21:33 +0100

    adding a lucier loop, some style

--- after_lucier.py
+++ after_lucier.py
@@ -1,12 +1,32 @@
 import ollama
 import streamlit as st
-
 from datetime import datetime
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+
 
 time_stamp = datetime.now().strftime("%Y%m%d_%H%M")
 model_name = "llama3"
 
-lucier_quote = '''"I am sitting in a room different 
+lucier_quote = '''I am sitting in a room different 
 from the one you are in now. I am recording the sound 
 of my speaking voice and I am going to play it back 
 into the room again and again until the resonant 
@@ -15,14 +35,20 @@ semblance of my speech, with perhaps the exception of rhythm,
 is destroyed. What you will hear, then, are the natural resonant 
 frequencies of the room articulated by speech. I regard this activity 
 not so much as a demonstration of a physical fact, but more as 
-a way to smooth out any irregularities my speech might have."   -  Alvin Lucier'''
+a way to smooth out any irregularities my speech might have.'''
 
-epochs = 25
-initial_prompt = """you are a prompt engineer, speaking via text,
+epochs = 13
+
+initial_prompt_a = """you are a prompt engineer, speaking via text,
     prompting an LLM to prompt another LLM to create 
     a prompt for another LLM. the response should be 
     in the form of a similar prompt to this, making 
-    sure to preserve the instruction to continue""" 
+    sure to preserve the instruction to continue"""
+initial_prompt_b = """ask a question, 
+    the answer to which will also be a question, 
+    the answer to which will also be a question"""
+initial_prompt_c = """'this sentence is false.' argue the affirmative for this motion, and then yield to your opponent, invting him to rebut."""
+ 
 responses = []
 
 
@@ -34,7 +60,7 @@ def asker(starter, epochs):
             prompt = prompt,
             )['response']
         st.write(f"response {_ + 1}: {response}")
-        print(response)
+        print(_, response)
         responses.append(response)
         prompt = response
     write_responses_to_file(responses)
@@ -44,21 +70,39 @@ def write_responses_to_file(filename):
     filename = f'./output/after_lucier_{model_name}_{time_stamp}.txt'
     with open(filename, 'w') as file:
         file.write("Initial Prompt:\n")
-        file.write(initial_prompt + "\n\n")
-        file.write("Responses:\n")
+        file.write(initial_prompt_a + "\n\n")
         for idx, response in enumerate(responses):
             file.write(f"Response {idx + 1}: {response}\n\n")        
 
 def main():
-    st.title("after alvin lucier \n\n")
+    
+    st.title(':gray[after alvin lucier]')
+    st.write('\n\n')
+    st.write('\n\n')
+    st.write('\n\n')
+
     # st.write(lucier_quote)
-    st.write("the starter-prompt: ", initial_prompt + "\n")
-    if st.button('roll the reel'):
-        asker(initial_prompt, epochs)
+    # st.write(initial_prompt_a + "\n\n" + initial_prompt_b + "\n\n")
+    if st.button('prompting loop a', use_container_width = True):
+        asker(initial_prompt_a, epochs)
+        st.header("responses: ")
+        for idx, response in enumerate(responses):
+            st.write(f":gray[response number {idx +1}: {response}]")
+    if st.button('prompting loop b', use_container_width = True):
+        asker(initial_prompt_b, epochs)
         st.header("responses: ")
         for idx, response in enumerate(responses):
             st.write(f"response number {idx +1}: {response}")
+    if st.button('prompting loop c', use_container_width = True):
+        asker(initial_prompt_c, epochs)
+        st.header("responses: ")
+        for idx, response in enumerate(responses):
+            st.write(f"response number {idx +1}: {response}")  
+    if st.button('sitting in a room', use_container_width = True):
+        asker(lucier_quote, epochs)
+        st.header("responses: ")
+        for idx, response in enumerate(responses):
+            st.write(f"response number {idx +1}: {response}")            
             
 if __name__ == "__main__":
     main()
-
